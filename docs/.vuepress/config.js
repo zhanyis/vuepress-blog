e4d1aacdad30378b8b830425a604eefb1f1ef548
szy <zhanyisu126@gmail.com>
2021-07-26 22:57:12 +0800

    fill icon navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,30 @@
 module.exports = {
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: 'SZY blog',
+  description: 'a blog manage by a lazy person',
   base: '/vuepress-blog/',
+  head: [['link', { rel: 'icon', href: 'iconStar.png' }]],
+
+  themeConfig: {
+    repo: 'https://github.com/zhanyis/vuepress-blog',
+    editLink: false,
+    navbar: [
+      {
+        text: '前端',
+        link: '/frontend',
+      },
+      {
+        text: '日常',
+        link: '/daily',
+      },
+    ],
+  },
+  plugins: [
+    [
+      '@vuepress/plugin-google-analytics',
+      {
+        id: 'G-HCDF3J2F1X',
+      },
+    ],
+  ],
 }
